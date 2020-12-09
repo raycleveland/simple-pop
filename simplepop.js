20b83b8124a652b0cbb796f54ff67257dd07404a
raycleveland <raycleveland@gmail.com>
2020-12-09 12:54:08 -0700

    Adding support for a showOnce parameter with cookie support

--- simplepop.js
+++ simplepop.js
@@ -10,8 +10,20 @@ let simplePop = {
 	 * Simple pop start  function
 	 *
 	 * integer 	delay 			the milisecond delay to display the popup
+	 * boolean 	showOnce 		whether or not to show the popup once
 	 */
-	start: function(delay) {
+	start: function(delay, showOnce) {
+
+		// show the popup only once if set
+		if(showOnce) {
+			if(simplePop.getCookie("spshown") == "") {
+				document.cookie = "spshown=true";
+			}
+			else {
+				return false;
+			}
+		}
+
 		if(!delay) {
 			simplePop.open();
 		}
@@ -39,6 +51,23 @@ let simplePop = {
 	close: function() {
 		var container = document.getElementById("simple-pop-wrap");
   		container.classList.add("hidden");
+	},
+	/**
+	 * getCookie Utility Function
+	 */
+	getCookie: function(cname) {
+		var name = cname + "=";
+		var decodedCookie = decodeURIComponent(document.cookie);
+		var ca = decodedCookie.split(';');
+		for(var i = 0; i <ca.length; i++) {
+		  var c = ca[i];
+		  while (c.charAt(0) == ' ') {
+		  c = c.substring(1);
+		  }
+		  if (c.indexOf(name) == 0) {
+		    return c.substring(name.length, c.length);
+		  }
+		}
+		return "";
 	}
-	
 };
